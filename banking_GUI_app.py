c50b4707794e6952cb08790299b64a198b46041e
Evieepk12 <eviepk12@gmail.com>
2022-02-10 22:03:33 +0700

    finished the project

--- banking_GUI_app.py
+++ banking_GUI_app.py
@@ -101,9 +101,116 @@ def login_session():
     login_notif.config(fg="red", text="No account found!!")
 
 def deposit():
-    print("deposit")
+    #Variables
+    global amount 
+    global deposit_notif
+    global current_balance_label
+
+    amount = StringVar()
+    file = open(login_username, "r")
+    file_data = file.read()
+    user_details = file_data.split("\n")
+    details_balance = user_details[4]
+
+    #Deposit window
+    deposit_screen = Toplevel(master)
+    deposit_screen.title("Deposit")
+
+    #label
+    Label(deposit_screen, text="Deposit", font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
+    current_balance_label = Label(deposit_screen, text=f"Current Balance: ${details_balance}", font=('Calibri', 12))
+    current_balance_label.grid(row=1, sticky=W)
+    Label(deposit_screen, text="Amount: ", font=('Calibri', 12)).grid(row=2, sticky=W)
+    deposit_notif = Label(deposit_screen, font=('Calibri', 12))
+    deposit_notif.grid(row=4, sticky=N, pady=5)
+
+    #Entry 
+    Entry(deposit_screen, textvariable=amount).grid(row=2, column=1)
+
+    #Buttons
+    Button(deposit_screen, text="Finish", font=('Calibri', 12), command=finish_deposit).grid(row=3, sticky=W, pady=5)
+
+def finish_deposit():
+    if amount.get() == "":
+        deposit_notif.config(text="Enter a valid amount to deposit!", fg="red")
+        return
+    elif float(amount.get()) <= 0:
+        deposit_notif.config(text="Negative currency is not accepted", fg="red")
+        return
+
+    file = open(login_username, 'r+')
+    file_data = file.read()
+    details = file_data.split('\n')
+    current_balance = details[4]
+    updated_balance = current_balance
+    updated_balance = float(updated_balance) + float(amount.get())
+    file_data = file_data.replace(current_balance, str(updated_balance))
+    file.seek(0)
+    file.truncate(0)
+    file.write(file_data)
+    file.close()
+
+    current_balance_label.config(text="Current Balance : $"+str(updated_balance), fg="green")
+    deposit_notif.config(text="Balance Updated", fg='green')
+
 def withdraw():
-    print("withdraw")
+    #Variables
+    global withdraw_amount 
+    global withdraw_notif
+    global current_balance_label
+
+    withdraw_amount = StringVar()
+    file = open(login_username, "r")
+    file_data = file.read()
+    user_details = file_data.split("\n")
+    details_balance = user_details[4]
+
+    #Deposit window
+    withdraw_screen = Toplevel(master)
+    withdraw_screen.title("Withdraw")
+
+    #label
+    Label(withdraw_screen, text="Deposit", font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
+    current_balance_label = Label(withdraw_screen, text=f"Current Balance: ${details_balance}", font=('Calibri', 12))
+    current_balance_label.grid(row=1, sticky=W)
+    Label(withdraw_screen, text="Amount: ", font=('Calibri', 12)).grid(row=2, sticky=W)
+    withdraw_notif = Label(withdraw_screen, font=('Calibri', 12))
+    withdraw_notif.grid(row=4, sticky=N, pady=5)
+
+    #Entry 
+    Entry(withdraw_screen, textvariable=withdraw_amount).grid(row=2, column=1)
+
+    #Buttons
+    Button(withdraw_screen, text="Finish", font=('Calibri', 12), command=finish_withdraw).grid(row=3, sticky=W, pady=5)
+
+def finish_withdraw():
+    if withdraw_amount.get() == "":
+        withdraw_notif.config(text="Enter a valid amount to deposit!", fg="red")
+        return
+    elif float(withdraw_amount.get()) <= 0:
+        withdraw_notif.config(text="Negative currency is not accepted", fg="red")
+        return
+
+    file = open(login_username, 'r+')
+    file_data = file.read()
+    details = file_data.split('\n')
+    current_balance = details[4]
+
+    if float(withdraw_amount.get()) > float(current_balance):
+        withdraw_notif.config(text="Insufficient Funds!", fg="red")
+        return
+
+    updated_balance = current_balance
+    updated_balance = float(updated_balance) - float(withdraw_amount.get())
+    file_data = file_data.replace(current_balance, str(updated_balance))
+    file.seek(0)
+    file.truncate(0)
+    file.write(file_data)
+    file.close()
+
+    current_balance_label.config(text=f"Current Balance : ${str(updated_balance)}", fg="green")
+    deposit_notif.config(text="Balance Updated", fg='green')
+
 def personal_details():
     #Variables
     file = open(login_username, 'r')
@@ -123,7 +230,7 @@ def personal_details():
     Label(personal_details_screen, text=f"Name: {details_username}", font=('Calibri', 12)).grid(row=1,sticky=W)
     Label(personal_details_screen, text=f"Age: {details_age}", font=('Calibri', 12)).grid(row=2,sticky=W)
     Label(personal_details_screen, text=f"Gender: {details_gender}", font=('Calibri', 12)).grid(row=3,sticky=W)
-    Label(personal_details_screen, text=f"Current Balance: {details_balance}", font=('Calibri', 12)).grid(row=4,sticky=W)
+    Label(personal_details_screen, text=f"Current Balance: ${details_balance}", font=('Calibri', 12)).grid(row=4,sticky=W)
 
 def login():
     #Variables
